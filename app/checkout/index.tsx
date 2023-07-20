3b2b0cebdca8f9e0ea58ec91922bf2c22141a164
izuchukwu-eric <onukwubeizu@gmail.com>
2023-07-20 09:00:03 +0100

    forms UI

--- app/checkout/index.tsx
+++ app/checkout/index.tsx
@@ -1,23 +1,39 @@
-import { View, Text } from "react-native"
+import { View, Text, ScrollView } from "react-native"
 import { useRouter } from "expo-router"
-import { Button } from "react-native-paper"
+import { Button, Card, TextInput, useTheme } from "react-native-paper"
 
 
 export default function PersonalDetails() {
     const router = useRouter();
+    const theme = useTheme();
 
     const nextPage = () => {
         router.push("/checkout/delivery")
     }
     return (
-        <View>
-            <Text>
-                Personal Details
-            </Text>
+        <ScrollView 
+            contentContainerStyle={{ gap: 15, maxWidth: 500, width: "100%", alignSelf: "center"  }} 
+            showsVerticalScrollIndicator={false}
+        >
+            <Card style={{ backgroundColor: theme.colors.background }}>
+                <Card.Title title="Personal infomation" titleVariant="titleLarge" />
+                <Card.Content style={{ gap: 15 }}>
+                    <TextInput 
+                        placeholder="Name"
+                        label="Name"
+                        style={{ backgroundColor: theme.colors.background }}
+                    />
+                    <TextInput 
+                        placeholder="hey@gmail.com"
+                        label="Email"
+                        style={{ backgroundColor: theme.colors.background }}
+                    />
+                </Card.Content>
+            </Card>
 
             <Button onPress={nextPage} mode="contained">
                 Next
             </Button>
-        </View>
+        </ScrollView>
     )
 }
--- app/checkout/payment.tsx
+++ app/checkout/payment.tsx
@@ -1,10 +1,11 @@
 import { useRouter } from "expo-router";
-import { View, Text } from "react-native"
-import { Button } from "react-native-paper";
+import { ScrollView, View } from "react-native"
+import { Button, Card, TextInput, useTheme, Checkbox } from "react-native-paper";
 
 
 export default function PaymentDetails() {
     const router = useRouter();
+    const theme = useTheme();
 
     const nextPage = () => {
         //submit
@@ -13,14 +14,36 @@ export default function PaymentDetails() {
     }
 
     return (
-        <View>
-            <Text>
-                Payment Details
-            </Text>
+        <ScrollView
+            contentContainerStyle={{ gap: 15, maxWidth: 500, width: "100%", alignSelf: "center" }} showsVerticalScrollIndicator={false}
+        >
+            <Card style={{ backgroundColor: theme.colors.background }} >
+                <Card.Title title={"Payment details"} titleVariant="titleLarge" />
+                <Card.Content style={{ gap: 10 }}>
+                    <TextInput 
+                        label={"Card number"}
+                        placeholder="4242 4242 4242 4242"
+                        style={{ backgroundColor: theme.colors.background }}
+                    />
+                    <View style={{ flexDirection: 'row', gap: 15 }} >
+                        <TextInput 
+                            label={"Expiration date"}
+                            placeholder="mm/yyyy"
+                            style={{ backgroundColor: theme.colors.background, flex: 3 }}
+                        />
+                        <TextInput 
+                            label={"Security code"}
+                            style={{ backgroundColor: theme.colors.background, flex: 2 }}
+                        />
+                    </View>
+
+                    <Checkbox.Item label="Save payment information" status="checked" />
+                </Card.Content>
+            </Card>
 
             <Button onPress={nextPage} mode="contained">
                 Submit
             </Button>
-        </View>
+        </ScrollView>
     )
 }
